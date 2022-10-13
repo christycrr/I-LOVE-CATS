6baf5b65f8df26521bba78c63e98fd9d3186a1e9
Christy Carr <ccarr9134@gmail.com>
2022-10-13 19:09:36 +0000

    Lab 6 Final Update JS

--- Index.js
+++ Index.js
@@ -1,15 +1,19 @@
-let myname=prompt("what is your name");
-document.write("welcome "+ myname)
-if (myname == "Christy")
-    {console.log("Hey girl");}
+let myname = prompt("what is your name");
+document.write("welcome " + myname)
+if (myname == "Christy") {
+    console.log("Hey girl");
+    document.write("Hey girl!");
+}else {
+    console.log("Greetings")
+    document.write("Cats are so cute")
+}
 
-else 
-    {console.log("Greetings")
-document.write("Cats are so cute")}
-    let guess = prompt ("What is my favorite number? 0-10") 
-    let favnumber = "7"
-    if (guess == 7) {alert ("That's it!")}
-if (guess = 7) {alert ("That's not it!")}
-document.write ("Who sent you?")
+let guess = prompt("What is my favorite number? 0-10")
+let favnumber = "7"
+if (guess == 7) {
+     alert("That's it!");
+     document.write ("You guessed my favorite number good job!")
+}
+if (guess !== 7) { alert("That's not it!") }
+document.write("Who sent you?")
 
-    
