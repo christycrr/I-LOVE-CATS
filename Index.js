3fa5ce02ccd95f0b219a08db0922dc1cd070f6d4
Christy Carr <ccarr9134@gmail.com>
2022-10-13 02:24:10 +0000

    Lab 6JS

---
+++ Index.js
@@ -0,0 +1,11 @@
+let myname=prompt("what is your name");
+alert("welcome "+ myname)
+if (myname == "Christy")
+    {console.log("Hey girl");}
+
+else 
+    {console.log("Greetings")}
+    let guess = prompt ("What is my favorite number? 0-10") 
+    let favnumber = "7"
+    if (guess = 7); {alert ("awesome!")}
+    
